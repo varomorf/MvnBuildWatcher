89fcf60f56f7fd23722bae10003048bcdaaede3b
Alvaro Fernandez <alvaro.fernandez@nortia-in.es>
2014-09-25 09:58:47 +0200

    Extract the addition of modules to the Maven build status object to the object itself

--- src/main/java/org/platypus/mvnWatcher/MvnBuildStatus.groovy
+++ src/main/java/org/platypus/mvnWatcher/MvnBuildStatus.groovy
@@ -13,16 +13,26 @@ class MvnBuildStatus {
 	// Attributes ----------------------------------------------------
 
 	/**List with the statuses of each of the modules of the build*/
-	List<MvnModuleBuildStatus> modulesStatus
+	List<MvnModuleBuildStatus> modulesStatus = []
 
 	/**Flag to mark whether the build is correct or not*/
-	boolean buildCorrect
+	boolean buildCorrect = false
 
 	// Static --------------------------------------------------------
 
 	// Constructors --------------------------------------------------
 
 	// Public --------------------------------------------------------
+
+	/**
+	 * Add a new module to the modules of this build (this will NOT look for duplicates) with 
+	 * {@link MvnModuleBuildStatus#WAITING} status
+	 * 
+	 * @param moduleName the name of the module to be added
+	 */
+	public void addNewModule(String moduleName){
+		modulesStatus.add new MvnModuleBuildStatus(moduleName: moduleName)
+	}
 
 	// Package protected ---------------------------------------------
 
